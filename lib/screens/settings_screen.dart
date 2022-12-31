322b430c6fd5ae38084da2cb654340cf6963a316
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-31 00:14:39 -0600

    Se cambia el tema

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/providers/theme_provider.dart';
 import 'package:preferences_app/share_preferences/preferences.dart';
 import 'package:preferences_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatefulWidget {
 
@@ -39,6 +41,11 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 title: const Text('Darkmode'),
                 onChanged: ( value ) {
                   Preferences.isDarkmode = value;
+                  final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+
+                  value 
+                  ? themeProvider.setDarkMode() 
+                  : themeProvider.setLightMode();
                   // redibuja el cambio de un valor
                   setState(() {});
                 }
